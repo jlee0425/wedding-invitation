73678eb9297471e6427270a4a89ad2c001661dd6
JL <jlee425kr@gmail.com>
2022-12-18 22:00:10 +0900

    Add: 헤드, 섹션 스타일링.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,19 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 import font from '../styles/font';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main className={`${font.variable} font-sans`}>
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>루나&정규 청첩장</title>
+        <meta name="description" content="Runa & Jeongkyu, Wedding Invitation Card" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className={`${font.variable} font-sans snap-y bg-amber-50 text-stone-700`}>
+        <Component {...pageProps} />
+      </main>
+    </>
   );
 }
